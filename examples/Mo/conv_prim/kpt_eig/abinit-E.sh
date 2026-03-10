7e351bb2d2d1702d8b2ab4ea35f00e00a1a215e6
suryaunm <suryabteja@unm.edu>
2026-03-10 13:54:57 -0600

    Add files via upload

---
+++ examples/Mo/conv_prim/kpt_eig/abinit-E.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --job-name MoNb50GS
+#SBATCH --account=2016234
+#SBATCH --partition debug
+#SBATCH --time 00:30:00
+##SBATCH --exclusive
+##SBATCH --mail-user=youremail@unm.edu
+
+# On Xena:
+module load abinit/10.4.5
+
+jobstart=`date`
+srun --mpi=pmi2 -n $SLURM_NTASKS abinit trf2_1.abi > trf2_1.log 2> trf2_1.err
+jobend=`date`
+echo "  abinit job start: $jobstart"
+echo $SLURM_NTASKS
+echo $SLURM_JOB_NODELIST
+echo "  abinit job end:   $jobend"
